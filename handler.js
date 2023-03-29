f3ba1b4dfe64a4f0f17e10478ba4ad055747bcdf
Default <55616945+Default-01@users.noreply.github.com>
2023-03-29 19:12:45 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -14,7 +14,6 @@ module.exports = async (client) => {
 	const eventFiles = await globPromise(`${process.cwd()}/modules/events/*.js`);
 	eventFiles.map((event) => {
 		const file = require(event);
-		client.on(file.event, (...args) => file.run(...args));
 		if (file.once) client.once(file.event, (...args) => file.run(...args));
 		else client.on(file.event, (...args) => file.run(...args));
 	});
