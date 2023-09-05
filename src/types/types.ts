1855e38ad87b4026eb0980fe47cbff19fe51a334
Renotje6 <38280916+Renotje6@users.noreply.github.com>
2023-09-05 14:46:06 +0200

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,13 @@
 // Types for log function
 export type LogType = 'debug' | 'info' | 'error' | 'success' | 'warn' | 'handler';
+
+// Add string prototype to replace welcomer variables
+declare global {
+	interface String {
+		replacer(replaceData: ReplaceData): string;
+	}
+}
+
+export interface ReplaceData {
+	[key: string | number | symbol]: unknown;
+}
